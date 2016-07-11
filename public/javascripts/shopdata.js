6d847bce1d004c551a563c1fa93c7a3deea93f59
tangdian <dean99dean@gmail.com>
2016-07-11 14:13:40 -0400

    add shangjia

--- public/javascripts/shopdata.js
+++ public/javascripts/shopdata.js
@@ -7,7 +7,7 @@ var bosses = {
 				time:"11AM - 1AM, Sunday: 12PM-1AM",
 				description:"15% off with membership card!",
 				phone:"(519) 208-0331",
-				imageUrl:"http://192.168.31.235:5500/logos/noon.png"
+				imageUrl:"http://ituwcssa.com:5500/logos/noon.png"
 			},
 			{
 				title: "Bogda",
@@ -16,8 +16,255 @@ var bosses = {
 				time:"11AM - 1AM, Sunday: 12PM-1AM",
 				description:"15% off with membership card!",
 				phone:"(519) 208-0331",
-				imageUrl:"http://192.168.31.235:5500/logos/bogda.png"
-			}
+				imageUrl:"http://ituwcssa.com:5500/logos/bogda.png"
+			},
+			{
+			title: "马队长拉面",
+			id: "ma",
+			address: "220 King Street North, Waterloo, ON",
+			imageUrl: "http://ituwcssa.com:5500/logos/ma.png",
+			time: "Mon-Sun: 10:00 am – 11:30 pm",
+			description: "现金 10% off",
+			phone: "519-957-9665"
+
+		},
+		{
+			title: "Foodie Fruitie",
+			id: "foodie",
+			address: "203 Lester St., Unit 7, Waterloo",
+			phone: "519-880-8688",
+			description: "10% Off (特价商品除外，代卖品牌和酒精饮料除外）",
+			imageUrl:"http://ituwcssa.com:5500/logos/foodie.png",
+			time: "Sun - Thurs: 11:30am - 11:30pm \
+			Fri, Sat: 11:30am - 12:00am \
+			*This is summer business hours,winter business hours will be back to regular hours",
+		},
+		{
+			title: "乡粤湘情 Empire Restaurant",
+			id: "empire",
+			address: "160 University Ave W, Waterloo",
+			phone: " 519-588-7777",
+			description: "现金 10% Off",
+			time: "Mon-Sun 10:00AM - 10:00PM"
+		},
+		{
+			title: "莲花烧烤",
+			id: "lianhua",
+			address: "150 University Ave W, Waterloo, ON N2L 6J3",
+			phone: "519-580-6666",
+			description: "现金 10% Off",
+			time: "Mon-Fri: 11:00 am – 10:00 pm\
+			Sat-Sun: 12:00 pm – 10:00 pm"
+		},
+		{
+			title: "Sogo (美食美客)",
+			id: "sogo",
+			address: "170 University Ave W, Waterloo",
+			phone: "519-208-2188",
+			description: "现金 10% Off",
+			time: "Mon-Sun: 10:00am-10:00pm"
+		},
+		{
+			title: "三十味",
+			id: "sanshi",
+			address: "465 Phillip St, Waterloo",
+			phone: "519-580-6666",
+			description: "10% Off",
+			imageUrl: "http://ituwcssa.com:5500/logos/sanshi.png",
+			time: "Mon-Fri: 11:00 am – 10:00 pm\
+			Sat-Sun: 12:00 pm – 10:00 pm"
+		},
+		{
+			title: "德庄(Morals Village)",
+			id: "moral",
+			address: "19-160  University Ave West, Waterloo",
+			phone: "519-954-8883",
+			description: "10% Off",
+			imageUrl: "http://ituwcssa.com:5500/logos/moral.png",
+			time: ""
+		},
+		{
+			title: "糖记 (Sugar Marmalade )",
+			id: "tangji",
+			address: "203 Lester Street Unit 5, Waterloo",
+			phone: "226-647-3382",
+			description: "糖记积分卡双倍积分",
+			imageUrl: "http://ituwcssa.com:5500/logos/tangji.png",
+			time: "12:00 pm – 2:00 am"
+
+		},
+		{
+			title: "Frat Burger",
+			id: "frat",
+			address: "1A&1B, 247 King Street North, Waterloo",
+			phone: "519-954-3728",
+			description: "10% off",
+			imageUrl: "http://ituwcssa.com:5500/logos/frat.png",
+			time: "Mon, Tues: 11AM - 10PM \
+			Wed, Thurs: 11AM - 11PM \
+			Fri, Sat: 11AM - 12AM \
+			Sun: 11AM - 11PM"
+		},
+		{
+			title: "Metro Restaurant",
+			id: "metro",
+			address: "168 Victoria Street North, Kitchener",
+			phone: "519-578-9189",
+			description: "10% off",
+			imageUrl : "http://ituwcssa.com:5500/logos/metro.png",
+			time: "周一  11am~8pm\
+			周二至周五 11am~9pm\
+			周六 12pm~9pm\
+			周日 4pm~8pm"
+
+		},
+		{
+			title: "宋师傅兰州拉面",
+			id:"song",
+			address: "D-220 King st N. Waterloo",
+			phone: "519-884-1188",
+			description: "现金 15% Off",
+			imageUrl:"http://ituwcssa.com:5500/logos/song.png",
+			time: ""
+
+		},
+		{
+			title: "食德喜小馆 (China Garden) ",
+			address: "31 University Ave E, Waterloo",
+			phone: "519-888-0088",
+			description: "Over 25 CAD: 10% Off",
+			time: "11am - 10pm, TUE closed"
+
+		},
+		{
+			title: "Sowon Korean Restaurant",
+			id: "sowon",
+			address: "150 University Ave W, Waterloo",
+			phone: "519-772-0146",
+			description: "Cash 10% Off",
+			time: ""
+
+		},
+		{
+			title: "恩康饺子馆",
+			id: "enkang",
+			address:"5B-150 University Avenue West, Waterloo",
+			time:"11AM - 1AM, Sunday: 12PM-1AM",
+			description:"15% off with membership card!",
+			phone:"519-208-0061",
+			imageUrl:"http://ituwcssa.com:5500/logos/enkang.png"
+		},
+		{
+			title: "石头剪刀铺",
+			id: "enkang",
+			address:"Unit A, 181 Lester Street, Waterloo",
+			time:"",
+			description:"Have a second chance to play rock paper scissors.",
+			phone:"",
+			imageUrl:"http://ituwcssa.com:5500/logos/rsp.png"
+		},
+		{
+			title: "Rolling Peppers",
+			id: "enkang",
+			address:"Unit4, 2 King st North, Waterloo; King&Erb; The Waterloo Hotel First Floor",
+			time:"",
+			description:"Cash 10% Off",
+			phone:"",
+			imageUrl:"http://ituwcssa.com:5500/logos/rollingpepper.png"
+		},
+		{
+			title: "品味居",
+			id: "pinweiju",
+			address:"258 King St N, Waterloo",
+			time:"",
+			description:"Cash 10% Off",
+			phone:"519-954-6477",
+			imageUrl:"http://ituwcssa.com:5500/logos/pinweiju.png"
+		},
+		{
+			title: "Bao",
+			id: "bao",
+			address: "62 Balsam St B106, Waterloo",
+			time:"",
+			description: "10% Off",
+			phone: "519-208-1226",
+			imageUrl: "http://ituwcssa.com:5500/logos/bao.png"
+		}
+	],
+	"Drink":[
+		{
+			title: "Noon Moment",
+			id:"noon", 
+			address: "220 King Street North, Waterloo",
+			phone: "519-208-0331",
+			description: "15% Off",
+			imageUrl:"http://ituwcssa.com:5500/logos/noon.png",
+			time: "Mon-Saturday: 11:00 am – 1:00 am\
+			Sunday: 12:00 pm – 1:00 am"
+		},
+		{
+			title: "Chatime",
+			id: "chatime",
+			address: "255 King St N #9, Waterloo",
+			phone: "519-208-5580",
+			description: "所有饮料 Free Topping",
+			imageUrl: "http://ituwcssa.com:5500/logos/chatime.png",
+			time: "Mon-Sun: 11:30AM - 12:00AM"
+		},
+	],
+	"Shopping": [
+	 {
+	 	title: "7 days Mini Mart (水城便利店）",
+		id: "shuicheng",
+		address: "",
+		phone: "Wechat: newpioneertravel",
+		description: "旅游产品9折，机票price match基础上$10 off",
+		time: "",
+		imageUrl:"http://ituwcssa.com:5500/logos/pioneer.png",
+	 },
+	 {
+	 	title: "McPhail’s Cycle & Sport",
+		id: "mcphail",
+		address: "98 King St. N, Waterloo",
+		phone: "519-886-4340",
+		description: "配件和冰球器材八五折，买原价新车赠送70元头盔或60元锁",
+		time: "",
+		imageUrl:"http://ituwcssa.com:5500/logos/mcphail.png",
+	 }
+	],
+	"Entertainment":[
+		{
+			title: "先锋旅游",
+			id: "pioneer",
+			address: "",
+			phone: "Wechat: newpioneertravel",
+			description: "旅游产品9折，机票price match基础上$10 off",
+			time: "",
+			imageUrl:"http://ituwcssa.com:5500/logos/pioneer.png",
+		},
+		{
+			title: "FunHouse",
+			id: "funhouse",
+			address: "First floor 208 sunview st., Waterloo",
+			phone: "",
+			description: "Membership card at Funhouse 15% off\
+			Purchase 5% off",
+			imageUrl: "http://ituwcssa.com:5500/logos/funhouse.png",
+			time: ""
+		},
+	],
+	"Service": [
+		{
+			title: "幻彩发艺",
+			id: "pioneer",
+			address: "62 Balsam St. H102，Waterloo",
+			phone: "226-929-0666",
+			description: "",
+			time: "Monday - Friday 11am-7pm \
+			Saturday 10am-7pm \
+			Sunday 12pm-5pm",
+			imageUrl:"",
+		}
 	]
 	
 };
