40ab380f3e4491ae3feb1e5639d46ed9eb795450
Dian Tang <diant@yahoo-inc.com>
2017-02-02 16:07:06 -0800

    add android

--- backend/services/noteservice.js
+++ backend/services/noteservice.js
@@ -1,7 +1,9 @@
 var APNToken = require('../../secret.js').APNtoken;
 var apn = require('apn');
 var apnProvider = new apn.Provider(APNToken);
+var ServerKey = require('../../secret.js').ServerKey;
 var note = new apn.Notification();
+var gcm = require('node-gcm');
 var logger = require('./logger');  
 
 
@@ -20,8 +22,31 @@ var noteservice = {
     apnProvider.send(note, dToken).then((result) => {
       logger.log("info", result);
     });
-	}
+	},
+
+  sendCommentNoteAndroid: function(dToken, postingId, comment) {
+    var message = new gcm.Message({
+    priority: 'high',
+      data: {
+          'type': 'comment',
+          'postingId': postingId,
+      },
+      notification: {
+          body: "\u2709 You have a new comment! \"" + comment + "\""
+      }
+    });
+
+    var sender = new gcm.Sender(ServerKey);
+    var registrationTokens = [];
+    registrationTokens.push(dToken);
+
+    sender.send(message, { registrationTokens: registrationTokens }, function (err, response) {
+      if(err) console.log("error", err);
+      else console.log("info", response);
+    });
+  }
 }
+
 
 
 module.exports = noteservice;
