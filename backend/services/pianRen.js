493c10ddd3d02e0c8db523f5254c3b95f4913a85
tangdian <dean99dean@gmail.com>
2017-04-01 10:35:57 -0700

    test

--- backend/services/pianRen.js
+++ backend/services/pianRen.js
@@ -7,6 +7,12 @@ mongoose.connect('mongodb://localhost/cssams');
 
 DBService.getNotifiableUsers(function(err, users) {
   console.log(users.length);
+  var tester = users.filter(function(user) {
+    return user.fname == 'isa';
+  })
+  console.log (tester);
+  // noteservice.sendCommentNote(user.deviceToken, posting._id, req.body.content);
+  // noteservice.sendCommentNoteAndroid(user.deviceToken, posting._id, req.body.content);
   // for (var i = 0; i < users.length; i++) {
   //   console.log(users[i].deviceToken);
   // }
