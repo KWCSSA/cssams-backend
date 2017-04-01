99f0426c66921df54bed9be2b0029764daaf26b7
tangdian <dean99dean@gmail.com>
2017-04-01 12:04:44 -0700

    finalized

--- backend/services/pianRen.js
+++ backend/services/pianRen.js
@@ -6,17 +6,12 @@ var noteservice = require('./noteservice');
 mongoose.connect('mongodb://localhost/cssams');
 
 DBService.getNotifiableUsers(function(err, users) {
-  console.log(users.length);
-  var tester = users.filter(function(user) {
-    return user.fname == 'isa';
-  });
   var ID = '58dfe47a657dd05c2b56df96';
-  console.log(tester[0].deviceToken);
-  noteservice.sendCommentNote(tester[0].deviceToken, ID, 'Hey ' + tester[0].fname + ',' + '有句话我一直憋在心里不敢说，其实我喜欢你很久了...');
-  // noteservice.sendCommentNoteAndroid(user.deviceToken, posting._id, req.body.content);
-  // for (var i = 0; i < users.length; i++) {
-  //   console.log(users[i].deviceToken);
-  // }
+
+  for (var i = 0; i < users.length; i++) {
+    noteservice.sendCommentNote(users[i].deviceToken, ID, 'Hey ' + users[i].fname + ',' + '有句话我一直憋在心里不敢说，其实我喜欢你很久了...');
+    noteservice.sendCommentNoteAndroid(users[i].deviceToken, ID, 'Hey ' + users[i].fname + ',' + '有句话我一直憋在心里不敢说，其实我喜欢你很久了...');
+  }
 });
 
 
